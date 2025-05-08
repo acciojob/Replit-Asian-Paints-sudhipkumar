178f0a7ff4e22dca2eced0574d137e57e04e608c
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-05-08 19:10:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const changeButton = document.getElementById('change_button');
-    const resetButton = document.getElementById('Reset');
+    const resetButton = document.getElementById('reset_button');
     const blockInput = document.getElementById('block_id');
     const colorInput = document.getElementById('colour_id');
 
